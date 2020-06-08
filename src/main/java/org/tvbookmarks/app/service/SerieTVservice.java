a512b66d0a6f63d06ef5edbfcf2882e09fcb0ec5
fpresente <presentefabri@gmail.com>
2020-06-08 17:26:15 +0200

    add DatSerieTV

--- src/main/java/org/tvbookmarks/app/service/SerieTVservice.java
+++ src/main/java/org/tvbookmarks/app/service/SerieTVservice.java
@@ -15,7 +15,10 @@ import org.springframework.stereotype.Service;
 
 import org.springframework.web.client.RestTemplate;
 import org.tvbookmarks.app.config.ApplicationProperties;
+import org.tvbookmarks.app.domain.DatiSerieTV;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -33,8 +36,8 @@ public class SerieTVservice {
     BaseURL=properties.getDbmovie().getBaseURL();
     }
 
-    public String findSerieTV(String name){
-        String result= null;
+    public List<DatiSerieTV> findSerieTV(String name){
+        List<DatiSerieTV> result= null;
 
         try {
             result=callDBMovieAPI(name);
@@ -46,23 +49,37 @@ public class SerieTVservice {
 
         return result;
     }
-    private String callDBMovieAPI(String name){
+    private List<DatiSerieTV> callDBMovieAPI(String name){
         String path="search/tv?api_key="+ ApiKey +"&query=";
         HttpHeaders headers = new HttpHeaders();
         RestTemplate template=new RestTemplate();
         ResponseEntity<String> responseEntity = template.exchange(BaseURL + path + name, HttpMethod.GET,
             new HttpEntity<>(headers), String.class);
-        String Json = responseConverter(responseEntity.getBody()).toString();
+        List<DatiSerieTV> Json = responseConverter(responseEntity.getBody());
         return Json;
     }
 
-    private JSONArray responseConverter(String response) {
+    private List<DatiSerieTV> responseConverter(String response) {
         log.info("errore "+ response);
 
         try {
             JSONObject resultJSON = new JSONObject(response);
-            JSONArray results = new JSONArray(resultJSON.getString("results"));
-            return results;
+            JSONArray resultsArray = new JSONArray(resultJSON.getString("results"));
+            List<DatiSerieTV> result=new ArrayList<>();
+
+            for(int i=0;i<resultsArray.length();i++){
+
+                resultJSON = resultsArray.getJSONObject(i);
+                DatiSerieTV datiSerieTV=new DatiSerieTV();
+                datiSerieTV.setName(resultJSON.getString("name"));
+                datiSerieTV.setOverview(resultJSON.getString("overview"));
+                datiSerieTV.setAvg_vote(resultJSON.getDouble("vote_average"));
+                datiSerieTV.setPopularity(resultJSON.getDouble("popularity"));
+
+                result.add(datiSerieTV);
+            }
+
+            return  result;
         } catch (JSONException e) {
             log.debug("Json exception!:" + e);
             return null;
--- src/main/java/org/tvbookmarks/app/web/rest/ExternalAPIController.java
+++ src/main/java/org/tvbookmarks/app/web/rest/ExternalAPIController.java
@@ -3,15 +3,20 @@ package org.tvbookmarks.app.web.rest;
 import netscape.javascript.JSObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.tvbookmarks.app.config.DatabaseConfiguration;
+import org.tvbookmarks.app.domain.DatiSerieTV;
 import org.tvbookmarks.app.service.SerieTVservice;
 
 
 import javax.validation.Valid;
+
+import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -19,7 +24,7 @@ import static org.springframework.http.ResponseEntity.ok;
  * Controller to call External API.
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api", produces ={ MediaType.APPLICATION_JSON_VALUE, "application/hal+json" })
 public class ExternalAPIController {
 
 
@@ -27,9 +32,9 @@ public class ExternalAPIController {
     private SerieTVservice service;
 
 @GetMapping("/search")
-public ResponseEntity<String> findTV(@RequestParam String name){
+public ResponseEntity<List<DatiSerieTV>> findTV(@RequestParam String name){
 
-    String response=service.findSerieTV(name);
+    List<DatiSerieTV> response=service.findSerieTV(name);
     return ok(response);
 
 }
